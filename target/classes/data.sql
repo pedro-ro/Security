75239a6829781e3aa494ad4bfe4ecfb9bf8022cd
Pedro Rodrigues <pedro.henrique@eprecise.com.br>
2022-11-19 14:13:33 -0300

    Projeto de Authenticação e Autorização JWT com Spring Security.

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO perfil values (1,'ROLE_ADMIN')
+INSERT INTO usuario values (1,'teste','$2a$10$akUHv1S9kHH4OppQn01Mr.eg9e480GI2H.TCWu6uOPTDUCpy8JOC6')
+INSERT INTO usuario values (2,'adm','adm')
